4b678950bd516218a634e5222f5fd3ae9bb2245a
Michael Chodolak <chodolak@gmail.com>
2017-12-29 19:50:38 -0800

    test cors

--- api/config/cors.php
+++ api/config/cors.php
@@ -12,7 +12,7 @@ return [
      |
      */
     'supportsCredentials' => true,
-    'allowedOrigins' => ['*'],
+    'allowedOrigins' => ['chodolak.com'],
     'allowedHeaders' => ['Content-Type', 'X-Requested-With', 'Origin', 'X-Auth-Token', 'Accept', 'Authorization'],
     'allowedMethods' => ['GET', 'POST', 'PUT', 'HEAD', 'OPTIONS', 'DELETE'],
     'exposedHeaders' => [],
